45ea4faf6af69324c5d586671bed9a85bb4f792d
Bohdan <62512974+BohdanBesarab@users.noreply.github.com>
2022-02-02 20:55:03 +0200

    Add files via upload

--- Query/SQLQuery3.sql
+++ Query/SQLQuery3.sql
@@ -196,7 +196,7 @@ ON
 	NAME = 'ShopDB',				
 	FILENAME = 'H:\ShopDB.mdf',	
 	SIZE = 30MB,                    
-	MAXSIZE = 100MB,				
+	MAXSIZE = 90MB,				
 	FILEGROWTH = 10MB				
 )
 LOG ON						  
@@ -208,4 +208,74 @@ LOG ON
 	FILEGROWTH = 4MB                   
 )               
 COLLATE Cyrillic_General_CI_AS
+
+USE ShopDB
+GO
+
+-----create tables
+
+CREATE TABLE Employees 
+(                                      
+	EmployeeId int NOT NULL, 
+	FName nvarchar (15) NOT NULL,
+	LName nvarchar (15) NOT NULL,
+	MName nvarchar (15) NOT NULL,
+	Salary money NOT NULL,
+	PriorSalary money NOT NULL,
+	HireDate date NOT NULL,
+	TerminationDate date NOT NULL,
+	ManageEmpId int NULL
+)
+GO		
+
+ALTER TABLE Employees ADD
+	CONSTRAINT PK_Employees PRIMARY KEY(EmployeeId)  -- add keys to Employees
+GO
+
+
+CREATE TABLE Customers 
+(
+	CustomerNo int NOT NULL IDENTITY,
+	FName nvarchar (15) NOT NULL,
+	LName nvarchar (15) NOT NULL,
+	MName nvarchar (15) NOT NULL,
+	Address1 nvarchar (50) NOT NULL,
+	Address2 nvarchar (50) NOT NULL,
+	City nchar (10) NOT NULL,
+	Phone char (12) NOT NULL,
+	DateInSystem date NULL
+)
+GO
+
+ALTER TABLE Customers ADD
+	CONSTRAINT PK_Customers PRIMARY KEY(CustomerNo)		-- add keys to Employees
+
+
+CREATE TABLE Orders 
+(
+	OrderID int NOT NULL IDENTITY,
+	CustomerNo int NOT NULL,
+	OrderDate date NULL,
+	EmployeeID int NULL
+)
+GO
+
+ALTER TABLE Orders ADD
+	CONSTRAINT PK_Orders PRIMARY KEY(OrderID)			-- add keys to Employees
+GO
+
+
+ALTER TABLE Orders ADD CONSTRAINT
+	FK_Orders_Customers FOREIGN KEY (CustomerNo)		--Orders_Customers
+	REFERENCES Customers(CustomerNo)
+GO
+
+ALTER TABLE Orders ADD CONSTRAINT
+	FK_Orders_Employees FOREIGN KEY (EmployeeId)		--Orders_Employees
+	REFERENCES Employees(EmployeeId)
+GO
+
+DROP TABLE Orders;
+DROP TABLE Customers;
+DROP TABLE Employees;
 
